9df80d2fa04034ab3bc3b5684a786614f8544e60
Alex Brinkman <abrink@commercekitchen.com>
2017-07-24 15:56:47 -0600

    [INIT] Adding Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,15 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Email settings
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000, only_path: false }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_caching = false
+  # config.action_mailer.delivery_method = :smtp
+
+  # To use url helpers in workers
+  Rails.application.routes.default_url_options = { host: "localhost", port: 3000, only_path: false }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
